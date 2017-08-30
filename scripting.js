59394d2f4c7e01a58473b984c5bcb4ff24d7db5a
humanforklift <fordosurveysite@gmail.com>
2017-08-30 13:23:50 +1200

    Updated all files

--- scripting.js
+++ scripting.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-var urls = [], streams = ["OgamingSC2", "esl_sc2", "storbeck", "freecodecamp", "cretetion", "habathcx", "kiichichaosreigns"], markupArr = [], online = [], offline = [];
+var streams = ["OgamingSC2", "esl_sc2", "TSM_Bjergsen", "Syndicate", "Reckful", "freecodecamp", "cretetion", "habathcx", "kiichichaosreigns"], markupArr = [], online = [], offline = [];
 
-
+//call getInfo function for each channel in streams array
 streams.forEach(getInfo);
 
-
-//retrieves API info for each stream and displays on the page
+//retrieves API info and displays on the page
 function getInfo(username) {
     //get channel information
     $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + username + "/?callback=?", function(data) {
@@ -46,80 +45,103 @@ function getInfo(username) {
     });
 }
 
-function search() {
-    var name = $(this).val();
-    console.log(name);
-/*$.ajax({
- type: 'GET',
- url: 'https://api.twitch.tv/kraken/search/channels?query=' + name,
- headers: {
-   'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
- },
- success: function(data) {
-   console.log(data);
- }
-});*/
-}
-//$(".search").on("keyup", search);
-
 //change active tab
 $("button").on("click", function() {
     $("button").removeClass("active");
     $(this).addClass("active");
 
-
     //change placeholder text depending on which tab is active
-    $("#search").hasClass("active") ? ($("input").attr("placeholder", "Let's see what else is on..."), $(".channels").hide(), $(".stream-box").css("padding-bottom", "10px"), $(".search").on("keyup", search)) : ($("input").attr("placeholder", "Search for one of your channels."), $(".channels").show(), $(".stream-box").css("padding-bottom", "0px"));
+    $("#search").hasClass("active") ? ($("input").attr("placeholder", "Let's see what else is on..."), $(".channels").hide(), $(".stream-box").css("padding-bottom", "10px")/*, $(".search").on("keyup", search)*/) : ($("input").attr("placeholder", "Search for one of your channels"), $(".channels").show(), $(".stream-box").css("padding-bottom", "0px"));
 
     //hide search box if online or offline tabs are active
-    $("#online").hasClass("active") || $("#offline").hasClass("active") ? $(".search-bar").css("visibility", "hidden") : $(".search-bar").css("visibility", "visible")
+    $("#online").hasClass("active") || $("#offline").hasClass("active") ? $(".search-bar").css("visibility", "hidden") : $(".search-bar").css("visibility", "visible");
+
 });
 
 //sort online streams
 $("#online").on("click", function() {
-        $(".channels").html(online);
+    $(".channels").html(online);
 });
 
 //sort offline streams
 $("#offline").on("click", function() {
-        $(".channels").html(offline);
+    $(".channels").html(offline);
 });
 
 //show all channels
 $("#all").on("click", function() {
+    $("input").val("");
+    $(".stream-box").css({"margin-top" : "auto"});
     markupArr.forEach(function(item) {
-        $(".channels").html(online);
-        $(".channels").append(offline);
+        $(".channels").html(online).append(offline);
     });
 });
+
+//function to open twitch stream
+function openTwitch(name) {
+    window.open("https://www.twitch.tv/" + name, "_blank");
+}
 
 //add click events to panels
 $(document).on("click", "div.panel", function() {
     var name = $(this).children(".name").html();
+    //remove panel if invalid channel
     if ($(this).children().hasClass("status error")) {
         $(this).remove();
+    //jump to channel in new window
     } else {
-        window.open("https://www.twitch.tv/" + name, "_blank");
+        openTwitch(name);
     }
 });
 
+//trigger filter function when text is typed in search bar
 $("input").keyup(filter);
 
 function filter() {
     var names = $(".name"), val = $("input").val().toLowerCase(), username, page = $(".channels");
-    console.log(val);
-    for (var i = 0; i < names.length; i++) {
-        username = names[i].innerHTML.toLowerCase();
-        console.log(username);
+    
+    //filter through channels in list if all tab is active
+    if ($(this).attr("placeholder") == "Search for one of your channels") {
+        for (var i = 0; i < names.length; i++) {
+            username = names[i].innerHTML.toLowerCase();
         
-        if (username.indexOf(val) != -1) {
-            $(".panel")[i].style.display = "";
-        } else {
-            $(".panel")[i].style.display = "none";
+            if (username.indexOf(val) != -1) {
+                $(".panel")[i].style.display = "";
+            } else {
+                $(".panel")[i].style.display = "none";
+            }
         }
-    }
-}
+    //search for new streams if find more streams tab is active
+    } else {
+        //enable autocomplete on search bar
+        $("input").autocomplete({
 
-console.log(urls);
+            source: function(request, response) {
+                $.ajax({
+                    type: 'GET',
+                    url: 'https://api.twitch.tv/kraken/search/channels?query=' + request.term,
+                    headers: {
+                        'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
+                    },
+                    success: function(data) {
+                        $(".stream-box").css({"margin-top" : "0px"});
+                        var nameArr = [];
+                        for (var i = 0; i < data.channels.length; i++) {
+                            nameArr.push(data.channels[i].display_name);
+                        }
+                        response(nameArr);
+                    }
+                });
+            },
+
+            //open twitch channel of clicked autocomplete item
+            select: function(event, ui) {
+                $("input").val(ui.item.value);
+                var channelName = $("input").val();
+                openTwitch(channelName);
+            }
+        });
+    }    
+}
 
 });
